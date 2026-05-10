f0e8ef185b71665b81a671ccee10a97782230aa1
Peter <89460630+Peter-Zhao-751@users.noreply.github.com>
2026-05-09 21:19:57 -0700

    Initial Commit

---
+++ Vibez/VibezApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibezApp.swift
+//  Vibez
+//
+//  Created by Peter Zhao on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct VibezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
